d39464511b02b34660ee41b2577e5580b12cf6a8
Dripper <cordeiromariano17@gmail.com>
2023-01-24 04:19:09 -0300

    terminado

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-let fecha = document.querySelector("#fecha");
+let fecha_container = document.querySelector(".fecha_container");
 let lista = document.querySelector("#lista");
 let input = document.querySelector("#input");
 let btnEnter = document.querySelector("#enter");
@@ -8,6 +8,44 @@ let uncheck = "bi-check-circle";
 let subrayado = "subrayado";
 let id;
 let listaElementos;
+
+let longitud;
+let latidud;
+
+let ciudad = document.getElementById("ciudad");
+let grados = document.getElementById("grados");
+let icono = document.getElementById("iconos");
+
+if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(posicion => {
+        longitud = posicion.coords.longitude;
+        latidud = posicion.coords.latitude;
+
+        let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latidud}&lon=${longitud}&lang=es&units=metric&appid=abad954ca549aad324470f40ca8c5315`;
+
+        fetch(url)
+            .then(Response => {return Response.json()})
+            .then(data => {
+                let ubicacion = data.name;
+                let temp = Math.round(data.main.temp);
+                let iconCode = data.weather[0].icon;
+                let iconoImagen = `http://openweathermap.org/img/wn/${iconCode}.png`;
+
+                let div = document.createElement("div");
+                div.classList.add("fecha");
+                div.innerHTML = `
+                                <span id="ciudad">${ubicacion}</span>
+                                <span id="grados">${temp}°C</span>
+                                <img src="${iconoImagen}" alt="" id="icono"></img>
+                                `;
+                fecha_container.append(div);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    })
+        
+}
 
 function agregarTarea(tarea, id , realizado, eliminado) {
 
